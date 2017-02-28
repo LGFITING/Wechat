6de948067d5f799a0176c769bdb29710a0df8079
louisxiv <502990436@qq.com>
2017-02-28 17:38:27 +0800

    授权测试

--- getUserInfo.php
+++ getUserInfo.php
@@ -8,17 +8,7 @@
 $appid = "wxd8e911e6cf0b7ed0";  
 $secret = "87dc05c99d168869fd9ecd6f213196ef";  
 $code = $_GET["code"];
-
-function getJson($url){
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE); 
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE); 
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    $output = curl_exec($ch);
-    curl_close($ch);
-    return json_decode($output, true);
-}
+ 
 //第一步:取全局access_token
 $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=$appid&secret=$secret";
 $token = getJson($url);
@@ -31,4 +21,19 @@ $oauth2 = getJson($oauth2Url);
 $access_token = $token["access_token"];  
 $openid = $oauth2['openid'];  
   print_r($openid);
+$get_user_info_url = "https://api.weixin.qq.com/cgi-bin/user/info?access_token=$access_token&openid=$openid&lang=zh_CN";
+$userinfo = getJson($get_user_info_url);
  
+//打印用户信息
+  print_r($userinfo);
+ 
+function getJson($url){
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE); 
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE); 
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $output = curl_exec($ch);
+    curl_close($ch);
+    return json_decode($output, true);
+}
