206130f18a606ded3ba5ce6042c55397079d9335
louisxiv <502990436@qq.com>
2017-02-28 18:56:28 +0800

    测试

--- getUserInfo.php
+++ getUserInfo.php
@@ -15,10 +15,11 @@ $oauth2 = getJson($oauth2Url);
 //第二步:根据全局access_token和openid查询用户信息  
 $access_token = $oauth2["access_token"];  
 $openid = $oauth2['openid'];  
+echo $access_token;
+echo $openid;exit();
 $get_user_info_url = "https://api.weixin.qq.com/cgi-bin/user/info?access_token=$access_token&openid=$openid&lang=zh_CN";
 
 $userinfo = getJson($get_user_info_url);
-var_dump($userinfo);exit();
 //打印用户信息
   print_r($userinfo);
  
