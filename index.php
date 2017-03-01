8ec3aefe2df2fe7c37bd6147956ec1dac9966ce3
louisxiv <502990436@qq.com>
2017-03-01 10:42:02 +0800

    ceshi

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ switch ($type) {
 $appid='wxd8e911e6cf0b7ed0';
 $redirect_uri = urlencode ( 'http://lg.im-rice.com/getUserInfo.php' );
 $url ="https://open.weixin.qq.com/connect/oauth2/authorize?appid=$appid&redirect_uri=$redirect_uri&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect";
-//header("Location:".$url);
+header("Location:".$url);
 
 //获取菜单操作:
    $menu = $weObj->getMenu();
