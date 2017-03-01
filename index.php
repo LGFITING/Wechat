6b27c43e9000b6f731efae802f1e69beb7c22bae
louisxiv <502990436@qq.com>
2017-03-01 14:23:53 +0800

    ceshi

--- index.php
+++ index.php
@@ -13,11 +13,11 @@ function get_current_url(){
     } 
     return $current_url; 
 }
-$url = get_current_url();
-echo $url;exit();
+$current_url = get_current_url();
+
 //scope=snsapi_base 实例
 $appid='wxd8e911e6cf0b7ed0';
-$redirect_uri = urlencode ( 'http://lg.im-rice.com/getUserInfo.php' );
+$redirect_uri = urlencode ( $current_url );
 $url ="https://open.weixin.qq.com/connect/oauth2/authorize?appid=$appid&redirect_uri=$redirect_uri&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect";
 header("Location:".$url);
 
