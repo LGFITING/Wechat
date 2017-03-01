88fc9e67b070011dc0f8bfa453d2e33c0daa4246
louisxiv <502990436@qq.com>
2017-03-01 17:52:52 +0800

    ceshi

--- index.php
+++ index.php
@@ -76,7 +76,7 @@ switch ($type) {
    		"button"=>
    			array(
    				array('type'=>'click','name'=>'最新消息','key'=>'MENU_KEY_NEWS'),
-  				array('type'=>'view','name'=>'我要搜索','url'=>$url),
+  				array('type'=>'view','name'=>'验证','url'=>$url),
    				)
  		);
    $result = $weObj->createMenu($newmenu);
