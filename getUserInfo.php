36dfaf3641e8f9aba632c54dad45202d9088b531
louisxiv <502990436@qq.com>
2017-03-01 09:43:43 +0800

    ceshi

--- getUserInfo.php
+++ getUserInfo.php
@@ -16,8 +16,7 @@ $oauth2 = getJson($oauth2Url);
 $access_token = $oauth2["access_token"];  
 $openid = $oauth2['openid']; 
 
-//$get_user_info_url = "https://api.weixin.qq.com/cgi-bin/user/info?access_token=$access_token&openid=$openid&lang=zh_CN";
-$get_user_info_url = "https://api.weixin.qq.com/sns/userinfo?access_token=$access_token&openid=$openid";
+$get_user_info_url = "https://api.weixin.qq.com/sns/userinfo?access_token=$access_token&openid=$openid&lang=zh_CN";
 $userinfo = getJson($get_user_info_url);
 //打印用户信息
   print_r($userinfo);
