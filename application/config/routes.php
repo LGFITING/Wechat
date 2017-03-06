71618cafba72b87142b59548946fd538cbc1b4d3
louisxiv <502990436@qq.com>
2017-03-06 16:56:06 +0800

    加入微信sdk

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,8 +49,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
-$route['404_override'] = '';
-$route['translate_uri_dashes'] = FALSE;
-//$route['default_controller'] = 'Wechat/index';
-//$route['(:any)'] = 'Wechat/index/$1';
+//$route['default_controller'] = 'welcome';
+//$route['404_override'] = '';
+//$route['translate_uri_dashes'] = FALSE;
+$route['default_controller'] = 'Wechatt/index';
+$route['(:any)'] = 'Wechatt/index/$1';
