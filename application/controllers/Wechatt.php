ee3320a07361bea48c03c42d3fae348c3d97a8e3
louisxiv <502990436@qq.com>
2017-03-06 17:05:43 +0800

    修改菜单

--- application/controllers/Wechatt.php
+++ application/controllers/Wechatt.php
@@ -9,15 +9,20 @@ class Wechatt extends CI_Controller {
     {
         $this->load->library('CI_Wechat');
         $menu = $this->ci_wechat->getMenu();
-          $newmenu =  array(
-   		"button"=>
-  			array(
-  				array('type'=>'click','name'=>'最新消息','key'=>'MENU_KEY_NEWS'),
-  				array('type'=>'view','name'=>'我要搜索','url'=>'http://www.baidu.com'),
-   				)
-		);
-  $result = $this->ci_wechat->createMenu($newmenu);
+        $newmenu = array(
+            "button" =>
+            array(
+                array('type' => 'click', 'name' => '最新消息', 'key' => 'MENU_KEY_NEWS'),
+                array('type' => 'view', 'name' => '我要搜索', 'url' => 'http://www.baidu.com'),
+            )
+        );
+        $result = $this->ci_wechat->createMenu($newmenu);
+        
+        $userMsg = $this->ci_wechat->getUserInfo();
+        $user = json_decode($userMsg);
+        var_dump($user);
     }
+
 }
 
 ?>
