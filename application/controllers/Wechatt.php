f0a440ae47c8bed0c112ef38c52490ab79226ed4
louisxiv <502990436@qq.com>
2017-03-06 17:11:12 +0800

    修改菜单

--- application/controllers/Wechatt.php
+++ application/controllers/Wechatt.php
@@ -18,23 +18,26 @@ class Wechatt extends CI_Controller {
         );
         $result = $this->ci_wechat->createMenu($newmenu);
 
+        $options = array(
+            'token' => 'LGwechat', //填写你设定的key
+            'encodingaeskey' => '' //填写加密用的EncodingAESKey，如接口为明文模式可忽略
+        );
+        $weObj = new Wechat($options);
+        $weObj->valid(); //明文或兼容模式可以在接口验证通过后注释此句，但加密模式一定不能注释，否则会验证失败
+        $type = $weObj->getRev()->getRevType();
+        switch ($type) {
+            case Wechat::MSGTYPE_TEXT:
+                $weObj->text("hello, I'm wechat")->reply();
+                exit;
+                break;
+            case Wechat::MSGTYPE_EVENT:
+                break;
+            case Wechat::MSGTYPE_IMAGE:
+                break;
+            default:
+                $weObj->text("help info")->reply();
+        }
 
-        $this->ci_wechat->valid(); //明文或兼容模式可以在接口验证通过后注释此句，但加密模式一定不能注释，否则会验证失败
-        $this->ci_wechat->getRev()->getRevType();
-        
-        switch($type) {
-	case Wechat::MSGTYPE_TEXT:
-			$this->ci_wechat->text("hello, I'm wechat")->reply();
-			exit;
-			break;
-	case Wechat::MSGTYPE_EVENT:
-			break;
-	case Wechat::MSGTYPE_IMAGE:
-			break;
-	default:
-			$this->ci_wechat->text("help info")->reply();
-}
-        
 //        $userMsg = $this->ci_wechat->getUserInfo();
 //        $user = json_decode($userMsg);
 //        var_dump($user);
