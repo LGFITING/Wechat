fb328fc95fa3c54801f1b32c7142f90a7ff5fb11
louisxiv <502990436@qq.com>
2017-03-06 17:01:00 +0800

    修改菜单

--- application/controllers/Wechatt.php
+++ application/controllers/Wechatt.php
@@ -8,20 +8,15 @@ class Wechatt extends CI_Controller {
     public function index()
     {
         $this->load->library('CI_Wechat');
-        $this->ci_wechat->test();
-    }
-
-    public function testWechat()
-    {
-        $this->load->library('Myclass');
-        $this->myclass->test();
-    }
-    /**
-     * 引入CI
-     */
-    public function includeWechat(){
-        $this->load->library('CI_Wechat');
-        $this->ci_wechat->test();
+        $menu = $this->ci_wechat->getMenu();
+          $newmenu =  array(
+   		"button"=>
+  			array(
+  				array('type'=>'click','name'=>'最新消息','key'=>'MENU_KEY_NEWS'),
+  				array('type'=>'view','name'=>'我要搜索','url'=>'http://www.baidu.com'),
+   				)
+		);
+  $result = $this->ci_wechat->createMenu($newmenu);
     }
 }
 
