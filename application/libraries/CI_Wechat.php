478a7dbe989db0fcf6de13254234c5923c5a6cf4
louisxiv <502990436@qq.com>
2017-03-07 17:30:38 +0800

    测试

--- application/libraries/CI_Wechat.php
+++ application/libraries/CI_Wechat.php
@@ -116,9 +116,9 @@ class CI_Wechat extends Wechat {
             return -1;
         } else {
             // 将粉丝的follow id 存入session中
-            $userinfo = $this->_CI->FollowModel->getUserInfoByOpenId($openid);
-
-            $follow_id = $this->_CI->session->userdata('follow_id');
+//            $userinfo = $this->_CI->FollowModel->getUserInfoByOpenId($openid);
+//
+//            $follow_id = $this->_CI->session->userdata('follow_id');
             if (!$follow_id) {
                 $this->_CI->session->set_userdata('follow_id', $userinfo['id']);
             }
